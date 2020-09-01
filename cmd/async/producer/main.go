50ad29a6a60a6c583826878d74ce4535b0878d17
Belinda Vennam <beemarie@us.ibm.com>
2020-09-01 15:27:20 -0400

    remove kafka code

--- cmd/async/producer/main.go
+++ cmd/async/producer/main.go
@@ -6,31 +6,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 	"time"
 
 	"github.com/bradleypeabody/gouuidv6"
 
-	"github.com/Shopify/sarama"
 	"github.com/go-redis/redis/v8"
 	"github.com/kelseyhightower/envconfig"
-	"knative.dev/eventing-contrib/kafka"
 )
 
 type EnvInfo struct {
-	Topics           string            `envconfig:"KAFKA_TOPICS"`
-	BootstrapServers []string          `envconfig:"KAFKA_BOOTSTRAP_SERVERS"`
-	Key              string            `envconfig:"KAFKA_KEY"`
-	Headers          map[string]string `envconfig:"KAFKA_HEADERS"`
-	Source           string            `envconfig:"SOURCE"`
-	RedisMaster      string            `envconfig:"REDIS_MASTER_NAME"`
-	Broker           string            `envconfig:"BROKER"`
 	StreamName       string            `envconfig:"REDIS_STREAM_NAME"`
-	Value            string
+	RedisAddress     string            `envconfig:"REDIS_ADDRESS"`
 }
 
 type RequestData struct {
@@ -89,13 +78,7 @@ func checkHeaderAndServe(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		var sourceErr error
-		if s.Source == "kafka" {
-			sourceErr = writeToKafka(r.Context(), s, reqJSON)
-		} else if s.Source == "redis" {
-			sourceErr = writeToRedis(r.Context(), s, reqJSON, reqData.ID)
-		}
-		if sourceErr != nil {
+		if sourceErr := writeToRedis(r.Context(), s, reqJSON, reqData.ID); sourceErr != nil {
 			w.WriteHeader(500)
 		} else {
 			w.WriteHeader(http.StatusAccepted)
@@ -105,60 +88,19 @@ func checkHeaderAndServe(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func writeToKafka(ctx context.Context, s EnvInfo, reqJSON []byte) (err error) {
-	fmt.Println("USING KAFKA")
-	// Create a Kafka client from our Binding.
-	client, err := kafka.NewProducer(ctx)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	producer, err := sarama.NewSyncProducerFromClient(client)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// Send the message this Job was created to send.
-	headers := make([]sarama.RecordHeader, 0, len(s.Headers))
-	for k, v := range s.Headers {
-		headers = append(headers, sarama.RecordHeader{
-			Key:   []byte(k),
-			Value: []byte(v),
-		})
-	}
-	partitionnum := rand.Int31n(3)
-	topicNum := rand.Int31n(10)
-	topics := strings.Split(s.Topics, ",")
-	partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
-		//Topic:   s.Topic,
-		Topic: topics[topicNum],
-		// Key:     sarama.StringEncoder(s.Key), //BMV TODO: What does key do?
-		Partition: partitionnum,
-		Value:     sarama.StringEncoder(reqJSON),
-		Headers:   headers,
-	})
-	if err != nil {
-		return err
-	}
-	log.Print(partition)
-	log.Print(offset)
-	return
-}
-
 func writeToRedis(ctx context.Context, s EnvInfo, reqJSON []byte, id string) (err error) {
-	fmt.Println("USING REDIS")
 	opts := &redis.UniversalOptions{
-		Addrs: []string{"redis.redis.svc.cluster.local:6379"},
+		Addrs: []string{s.RedisAddress},
 	}
 	client := redis.NewUniversalClient(opts)
-	fmt.Println("PUSHING ONTO QUEUE", reqJSON)
-	// TODO: maybe there's a different way to format the stream addition?
+	// TODO: maybe there's a different way to format the stream addition to prevent
+	// parsing issues in consumer?
 	strCMD := client.XAdd(ctx, &redis.XAddArgs{
 		Stream: s.StreamName,
 		Values: map[string]interface{}{
 			"data": reqJSON,
 		},
 	})
-	// rpush := client.RPush(ctx, "queuename", reqJSON)
 	if strCMD.Err() != nil {
 		log.Printf("Failed to publish %q %v", id, strCMD.Err())
 		return strCMD.Err()
